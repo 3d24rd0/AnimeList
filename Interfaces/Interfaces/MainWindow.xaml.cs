9a5dbcf0fe4cae3e94ced6a66d6e3f1fc89e2ebc
Eduardo <eduardo.martinez.catala@gmail.com>
2014-03-16 10:39:43 +0100

    empezando

---
+++ Interfaces/Interfaces/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace Interfaces
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            this.Hide();
+            
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            ShowCustomBalloon();
+        }
+
+        private void Accesibilidad_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //ShowCustomBalloon();
+            
+        }
+        private void ShowCustomBalloon()
+        {
+            ToolTip a = new ToolTip();
+
+            //show balloon and close it after 4 seconds
+            tasckIcon.ShowCustomBalloon(a, PopupAnimation.Slide, 4000);
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            
+        }
+
+    }
+}
---
+++ Interfaces/servicio/Service1.svc.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using Oracle.DataAccess.Client;
+
+namespace servicio
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Service1 : IService1
+    {
+        private OracleConnection con;
+        private void Connect()
+        {
+            con = new OracleConnection();
+            con.ConnectionString = "User Id=edaurdo;Password=eduardo;Data Source=eduardo";
+            con.HostName = "192.168.0.10";
+            con.Open();
+            
+            Console.WriteLine("Connected to Oracle" + con.ServerVersion);
+        }
+        private void Close()
+        {
+            con.Close();
+            con.Dispose();
+        }
+
+        public Boolean Test()
+        {
+            try
+            {
+                Connect();
+                Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        } 
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
